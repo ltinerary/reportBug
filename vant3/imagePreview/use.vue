e461377c4e849a1737cae3b5bf7d6f531438459a
fan <hdu424fan@outlook.com>
2022-10-11 12:54:41 +0800

    Create use.vue

---
+++ vant3/imagePreview/use.vue
@@ -0,0 +1,20 @@
+const prviewImage = (key) => {
+    // let allKey = needPic('all');
+    let index = imageKey.value.indexOf(key);
+    if (index != -1) {
+        let resultArr = []
+        imageKey.value.forEach(key => {
+            resultArr.push(resultAllFileList[key])
+        })
+        console.log('当前预览的Key',key,index)
+        console.log('能够预览的图片key==>',imageKey.value)
+        console.log('能够预览的图片',resultArr)
+        ImagePreview({
+            images: resultArr,
+            startPosition: index,
+            closeable: true,
+        })
+    } else {
+        Toast(`没有找到您要预览的大图${key}`)
+    }
+}
